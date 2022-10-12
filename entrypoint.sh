b259ffc318e8b41f7f6c5d03f3faf1e8f0f33e5d
kscot <115618658+kscot@users.noreply.github.com>
2022-10-12 01:36:55 -0700

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# config xray
+cat << EOF > /etc/config.json
+{
+  "inbounds":[
+    {
+      "port": $PORT,
+      "protocol": "vmess",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "93a01817-459a-4733-8ac1-f9ce72ca225b"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}	
+EOF
+
+# run xray
+/usr/bin/xray run -config /etc/config.json
